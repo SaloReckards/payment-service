713da43409388ba57b26e4550a404c1a32202e6a
sergey <ser9227@yandex.ru>
2025-07-28 23:08:41 +0300

    add commands for task 8

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -1,36 +1,39 @@
 package com.iprody.payment.service.app.controller;
 
 import com.iprody.payment.service.app.persistence.entity.Payment;
+import com.iprody.payment.service.app.persistency.PaymentFilter;
+import com.iprody.payment.service.app.persistency.PaymentFilterFactory;
 import com.iprody.payment.service.app.persistency.PaymentRepository;
+import com.iprody.payment.service.app.persistency.PaymentSpecifications;
+import com.iprody.payment.service.app.services.PaymentService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 import java.util.*;
 
 @RestController
-@RequestMapping("/payments")
+@RequestMapping("/api/payments")
 public class PaymentController {
-    private PaymentRepository repository;
+    private final PaymentService paymentService;
 
-    public PaymentController(PaymentRepository repository) {
-        this.repository = repository;
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+    @GetMapping("/search")
+    public Page<Payment> searchPayments(@ModelAttribute PaymentFilter filter,
+                                        @RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "25") int size,
+                                        @RequestParam(defaultValue = "createdAt") String sortBy,
+                                        @RequestParam(defaultValue = "desc") String direction) {
+        Sort sort = direction.equalsIgnoreCase("desc")
+                ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return paymentService.searchPaged(filter, pageable);
     }
 
-    @GetMapping("/{guid}")
-    public Payment getPayments(@PathVariable UUID guid) {
-        return repository.findById(guid).orElse(null);
-    }
-
-    @GetMapping
-    public List<Payment> getAllPayments() {
-        return repository.findAll();
-    }
-
-    @PostMapping
-    public Payment createPayment(@RequestBody Payment payment) {
-        payment.setGuid(UUID.randomUUID());
-        payment.setCreatedAt(OffsetDateTime.now());
-        payment.setUpdatedAt(OffsetDateTime.now());
-        return repository.save(payment);
-    }
 }
