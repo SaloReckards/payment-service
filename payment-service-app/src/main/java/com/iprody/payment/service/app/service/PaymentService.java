8ef1454761d9ea32c9ac095516a23c7aff844726
sergey <ser9227@yandex.ru>
2025-08-10 13:46:37 +0400

    add CRUD operations to service classes

--- payment-service-app/src/main/java/com/iprody/payment/service/app/service/PaymentService.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/service/PaymentService.java
@@ -2,6 +2,8 @@ package com.iprody.payment.service.app.service;
 
 import com.iprody.payment.service.app.dto.PaymentDto;
 import com.iprody.payment.service.app.persistency.PaymentFilter;
+import com.iprody.payment.service.app.persistency.entity.PaymentStatus;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,4 +13,14 @@ public interface PaymentService {
     PaymentDto get(UUID id);
 
     Page<PaymentDto> search(PaymentFilter filter, Pageable pageable);
+
+    void delete(UUID id);
+
+    PaymentDto create(PaymentDto dto);
+
+    PaymentDto update(UUID id, PaymentDto dto);
+
+    PaymentDto updateNote(UUID id, @NotNull String note);
+
+    PaymentDto updateStatus(UUID id, @NotNull PaymentStatus status);
 }
