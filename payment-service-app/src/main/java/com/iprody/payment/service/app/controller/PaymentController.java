4b6864e3bfb0f975c2e0837d4bfb1e047a440f4c
sergey <ser9227@yandex.ru>
2025-08-16 12:25:46 +0400

    add @PreAuthorize to controller class

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -25,35 +26,41 @@ public class PaymentController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('admin')")
     public PaymentDto create(@RequestBody PaymentDto paymentDto) {
         return paymentServiceImpl.create(paymentDto);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     @ResponseStatus(HttpStatus.OK)
     public PaymentDto get(@PathVariable UUID id) {
         return paymentServiceImpl.get(id);
     }
 
     @GetMapping("/search")
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     @ResponseStatus(HttpStatus.OK)
     public Page<PaymentDto> search(PaymentFilter filter, Pageable pageable) {
         return paymentServiceImpl.search(filter, pageable);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('admin')")
     @ResponseStatus(HttpStatus.OK)
     public PaymentDto update(@PathVariable UUID id, @RequestBody PaymentDto paymentDto) {
         return paymentServiceImpl.update(id, paymentDto);
     }
 
     @PatchMapping("/{id}/status")
+    @PreAuthorize("hasRole('admin')")
     @ResponseStatus(HttpStatus.OK)
     public PaymentDto updateStatus(@PathVariable UUID id, @RequestBody @Valid PaymentStatusUpdateDto dto) {
         return paymentServiceImpl.updateStatus(id, dto.getStatus());
     }
 
     @PatchMapping("/{id}/note")
+    @PreAuthorize("hasRole('admin')")
     @ResponseStatus(HttpStatus.OK)
     public PaymentDto updateNote(@PathVariable UUID id, @RequestBody @Valid PaymentNoteUpdateDto dto) {
         return paymentServiceImpl.updateNote(id, dto.getNote());
@@ -61,6 +68,7 @@ public class PaymentController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('admin')")
     public void delete(@PathVariable UUID id) {
         paymentServiceImpl.delete(id);
     }
